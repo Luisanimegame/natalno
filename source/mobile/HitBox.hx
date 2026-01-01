8c962f90c59f5f2924570ae3a265d8f6202c0a4f
GaboWuz ★ <96131201+Luisanimegame@users.noreply.github.com>
2025-12-31 21:54:56 -0300

    aa

--- source/mobile/HitBox.hx
+++ source/mobile/HitBox.hx
@@ -38,10 +38,10 @@ class HitBox extends FlxSpriteGroup
     {
         var button:FlxButton = new FlxButton(x, y);
         button.makeGraphic(width, height, FlxColor.fromString(color));
-        button.alpha = 0.01;
+        button.alpha = 0.001;
 
-        button.onDown.callback = () -> button.alpha = 0.1;
-        button.onUp.callback = () -> button.alpha = 0.01;
+        button.onDown.callback = () -> button.alpha = 0.001;
+        button.onUp.callback = () -> button.alpha = 0.001;
         button.onOut.callback = button.onUp.callback;
 
         return button;
